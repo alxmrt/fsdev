920ad0a1da3a715bb81051af16bce8fc2d94b121
alxmrt <alxmrt@users.noreply.github.com>
2020-04-09 01:19:02 -0400

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -17,7 +17,7 @@ ADDONTITLE = '[COLOR limegreen][B]FS[/B][/COLOR]Dev'
 BUILDERNAME = 'FSDev'
 EXCLUDES = [ADDON_ID, 'repository.openwizard']
 # Text File with build info in it.
-BUILDFILE = 'http://'
+BUILDFILE = 'https://raw.githubusercontent.com/alxmrt/fsdev/master/resources/text/builds.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
